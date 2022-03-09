9b40113f44c7e274b1d0a3ea1b3a5960a64a0326
Erwan PETREMOUL <erwan.petremoul2021@campus-eni.fr>
2022-03-09 12:16:37 +0100

    navbar

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,12 +9,34 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="app_admin")
+     * @Route("/admin/villes", name="app_admin_villes")
      */
-    public function index(): Response
+    public function updateCity(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/AdminCity.html.twig');
+    }
+
+    /**
+     * @Route("/admin/campus", name="app_admin_campus")
+     */
+    public function updateCampus(): Response
+    {
+        return $this->render('admin/AdminCampus.html.twig');
+    }
+
+    /**
+     * @Route("/admin/outings", name="app_admin_outings")
+     */
+    public function updateOutings(): Response
+    {
+        return $this->render('admin/AdminOuting.html.twig');
+    }
+
+    /**
+     * @Route("/admin/participants", name="app_admin_participants")
+     */
+    public function updateParticipants(): Response
+    {
+        return $this->render('admin/AdminParticipants.html.twig');
     }
 }
