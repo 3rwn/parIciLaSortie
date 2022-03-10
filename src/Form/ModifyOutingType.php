b8ce1e030f7d2395e492f4f2f0e546bae7a4e58f
Margaux MARTINEAU <margaux.guibet@gmail.com>
2022-03-10 11:49:21 +0100

    modification ShowOuting et continue ModifyOuting

--- src/Form/ModifyOutingType.php
+++ src/Form/ModifyOutingType.php
@@ -2,8 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Location;
 use App\Entity\Outing;
+use App\Entity\State;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +19,46 @@ class ModifyOutingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-        //     ->add('dateTimeStartOuting')
-        //   ->add('duration')
-        //    ->add('registrationDeadLine')
-        //  ->add('maxRegistrations')
-            ->add('description')
-        //    ->add('participants')
-        //    ->add('organizer')
-        //    ->add('state')
-        //    ->add('location')
-            ->add('campus', null, ['choice_label' => 'name'])
+            ->add('name',null, ['label' => 'Nom de la sortie', 'attr' => ['pattern' => "[a-zA-Z]{1,50}"]])
+
+            ->add('dateTimeStartOuting', DateTimeType::class,[
+                'label' => 'Date et heure de la sortie',
+                'widget' => 'single_text'
+            ])
+
+            ->add('registrationDeadLine' ,DateType::class,[
+                'label' => 'Date fin d inscription',
+                'widget' => 'single_text'
+                ])
+
+            ->add('maxRegistrations', IntegerType::class,[
+                    'label' => 'Nombre de place'
+                ])
+
+            ->add('duration', IntegerType::class,[
+                'label' => 'Durée'
+            ])
+            ->add('description', TextareaType::class)
+        
+          ->add('campus', null, ['choice_label' => 'name'])  
+
+          ->add('location', EntityType::class,[
+            'class'=>Location::class,
+            'choice_label'=>'name',
+            'label' => 'Lieu'
+             ])
+
+          
+
+
+        //  ->add('participants')
+        //  ->add('organizer')
+        //->add('state', EntityType::class,[
+        //      'class'=>State::class,
+        //     'choice_label'=>'wording'])
+        
+       
+        
         ;
     }
 
